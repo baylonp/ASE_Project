7f0f901a4b3fb922ec51e1b6ce0249a0a45868fa
Bitrath <bitrath@users.noreply.github.com>
2024-12-02 01:28:06 +0100

    Token: Admin/User Distinction. Services: Admin, Auth, Auction are OK

--- gachas/authentication/app.py
+++ gachas/authentication/app.py
@@ -77,33 +77,69 @@ def token_required(f):
         try:
             # Decode HTTP Received Token
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-            current_user = User.query.filter_by(id=data['user_id']).first()
-            if current_user is None:
-                raise jwt.InvalidTokenError
-            # Extract User JWT Token From the UserDB
-            if (current_user.jwt_token is None):
-                return jsonify({'message': 'Forbidden Token for Logged OUT User!'}), 403
-            # Decode User JWT to check for Errors
-            tokenUser = jwt.decode(current_user.jwt_token, app.config['SECRET_KEY'], algorithms=['HS256'])
-            # Check The Two Tokens Signatures
-            if not compare_two_jwt(token, current_user.jwt_token, data, tokenUser):
-                return jsonify({'message': 'Forbidden Token for Logged IN User!'}), 403
-            # We are OK.
+            
+            # Verifica se è un token utente o admin
+            if 'user_id' in data:
+                current_user = User.query.filter_by(id=data['user_id']).first()
+                if current_user is None:
+                    raise jwt.InvalidTokenError
+                # Extract User JWT Token From the UserDB
+                if (current_user.jwt_token is None):
+                    return jsonify({'message': 'Forbidden Token for Logged OUT User!'}), 403
+                # Decode User JWT to check for Errors
+                tokenUser = jwt.decode(current_user.jwt_token, app.config['SECRET_KEY'], algorithms=['HS256'])
+                # Check The Two Tokens Signatures
+                if not compare_two_jwt(token, current_user.jwt_token, data, tokenUser):
+                    return jsonify({'message': 'Forbidden Token for Logged IN User!'}), 403
+                # (User) We are OK.
+                return f(current_user, token, *args, **kwargs)  # Passa il current_user e il token come argomento
+            elif 'admin_id' in data:
+                # send a request to the admin service to verify the token.
+                # Logica per token admin
+                username = data['username']
+                
+                # Effettua una richiesta HTTP al servizio esterno
+                admin_service_url = "https://admin_service:5000/admin_service/verify_admin"
+                payload = {'admin_username': username}
+                try:
+                    response = requests.get(admin_service_url, headers={'x-access-token': token}, verify=False)
+                    # Ensure status code is 200
+                    if response.status_code != 200:
+                        return jsonify(response.json()), 403
+                    
+                    if 'application/json' not in response.headers.get('Content-Type', ''):
+                        return jsonify({'message': 'Invalid response format from admin service!', 'details': response.text}), 500
+                    
+                    # Parse the JSON body
+                    admin_data = {
+                        'username': username
+                    }
+                    
+                    return f(admin_data, token, *args, **kwargs)
+                except requests.exceptions.RequestException as e:
+                    # Gestisci errori durante la richiesta HTTP
+                    return jsonify({'message': 'Error validating admin token!', 'error': str(e)}), 500
         except jwt.ExpiredSignatureError:
             return jsonify({'message': 'Token has expired!'}), 401
         except jwt.InvalidTokenError:
             return jsonify({'message': 'Token is invalid!'}), 401
-        return f(current_user, token, *args, **kwargs)  # Passa il current_user e il token come argomento
     return decorated
  
 # A Service has required the validation of a given Token
 @app.route("/authentication/validate", methods=["GET"])
-def validate_service_token():
+@token_required
+def validate_service_token(curr_user, token):
     """Endpoint to validate JWT token."""
+    # check for @token_required unseen errors
+    if curr_user is None or token is None: 
+        return make_response(jsonify({'message': 'Bad Request. Something happened on Token Vaerification.'}), 400) # Bad Request
+    
+    # @token_required has verified that we have received a valid token.
+    return make_response(jsonify({'message': 'Ok.'}), 200) # OK
+    '''
     token = request.headers.get("x-access-token")
     if token is None:
         return jsonify({"error": "Authentication Service ha received an Invalid token"}), 401
- 
     try:
         payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
         return jsonify({"user": payload})  # Include the user data in the response
@@ -111,6 +147,7 @@ def validate_service_token():
         return jsonify({"error": "Token has expired"}), 401
     except jwt.InvalidTokenError:
         return jsonify({"error": "Invalid token"}), 401
+    '''
  
 # Definizione del modello User
 class User(db.Model):
@@ -152,7 +189,6 @@ def create_account():
  
     return make_response(jsonify({'message': 'Account created successfully'}), 201)
  
- 
 # Endpoint per il login e la generazione del token JWT
 @app.route('/authentication/auth', methods=['POST'])
 def login():
@@ -171,17 +207,13 @@ def login():
     return make_response(jsonify({'message': 'Invalid credentials'}), 401)
  
  # Endpoint per il Logout di un User
+
 @app.route('/authentication/logout', methods=['PATCH'])
 @token_required
 def logout(current_user, token):
     account_id = request.args.get('accountId')
     if not account_id:
         return make_response(jsonify({'message': 'Account ID is required'}), 400)
- 
-    # Check if the Token UserId matches (AccountID)
-    # 403: Forbidden
-    if (current_user.id != int(account_id)): 
-        return make_response(jsonify({'message': 'AccountID Invalid. You are not authorized.'}), 403)
  
     # Find the user by ID and clear the jwt_token field
     user = User.query.get(account_id)
@@ -199,15 +231,6 @@ def delete_account(current_user, token):
     account_id = request.args.get('accountId')
     if not account_id:
         return make_response(jsonify({'message': 'Account ID is required'}), 400)
- 
-    # Check if the Token UserId matches the one received (AccountID)
-    # 403: Forbidden
-    if (current_user.id != int(account_id)): 
-        return make_response(jsonify({
-    'message': 'PlayerID Invalid. You are not authorized.',
-    'accountID': account_id,  # Example additional field
-    'currUserID': current_user.id   # Another example additional field
-}), 403)
  
     # Verifica se l'utente esiste nel database
     user = User.query.get(account_id)
@@ -241,18 +264,12 @@ def delete_account(current_user, token):
     except Exception as e:
         return make_response(jsonify({'message': f'An internal error occurred: {str(e)}'}), 500)
  
- 
 @app.route('/authentication/account', methods=['PATCH'])
 @token_required
 def update_account(current_user, token):
     account_id = request.args.get('accountId')
     if not account_id:
         return make_response(jsonify({'message': 'Account ID is required'}), 400)
- 
-    # Check if the Token UserId matches (AccountID)
-    # 403: Forbidden
-    if (current_user.id != int(account_id)): 
-        return make_response(jsonify({'message': 'AccountID Invalid. You are not authorized.'}), 403)
  
     user = User.query.get(account_id)
     if not user:
@@ -279,7 +296,6 @@ def update_account(current_user, token):
     db.session.commit()
     return make_response(jsonify({'message': 'Account updated successfully'}), 200)
  
- 
 @app.route('/authentication/userId', methods=['GET'])
 @token_required
 def get_user_id(current_user, token):
@@ -292,22 +308,11 @@ def get_user_id(current_user, token):
     if not user:
         return make_response(jsonify({'message': 'User not found'}), 404)
  
-    # Check if the Token UserId matches (user.id)
-    # 403: Forbidden
-    if (current_user.id != user.id): 
-        return make_response(jsonify({'message': 'UserID Invalid. You are not authorized.'}), 403)
- 
     return jsonify({'userId': user.id}), 200
- 
- 
+  
 @app.route('/authentication/players/<playerId>', methods=['GET'])
 @token_required
 def get_user_info(current_user, token, playerId):
-    # Check if the Token UserId matches (PlayerID)
-    # 403: Forbidden
-    if (current_user.id != int(playerId)): 
-        return make_response(jsonify({'message': 'PlayerID Invalid. You are not authorized.'}), 403)
- 
     try:
         # Recuperare l'utente dal database in base al playerId
         user = User.query.filter_by(id=playerId).first()
@@ -327,22 +332,21 @@ def get_user_info(current_user, token, playerId):
  
     except Exception as e:
         return make_response(jsonify({'message': f'An error occurred: {str(e)}'}), 500)
- 
- 
+
 @app.route('/authentication/players/<playerId>/currency/add', methods=['POST'])
 @token_required
 def add_currency_to_player(current_user, token, playerId):
     """
     Aggiunge monete di gioco al wallet del giocatore specificato
     """
-    # Check if the Token UserId matches (PlayerID)
+    '''# Check if the Token UserId matches (PlayerID)
     # 403: Forbidden
     if (current_user.id != int(playerId)): 
         return make_response(jsonify({
     'message': 'PlayerID Invalid. You are not authorized.',
     'playerID': playerId,  # Example additional field
     'currUserID': current_user.id   # Another example additional field
-}), 403)
+}), 403)'''
  
     try:
         # Ottieni l'importo specificato dall'utente tramite la query string
@@ -367,17 +371,12 @@ def add_currency_to_player(current_user, token, playerId):
     except Exception as e:
         return make_response(jsonify({'message': f'An error occurred: {str(e)}'}), 500)
  
- 
 @app.route('/authentication/players/<playerId>/currency/subtract', methods=['PATCH'])
 @token_required
 def subtract_currency_from_player(current_user, token, playerId):
     """
     Sottrae monete di gioco dal wallet del giocatore specificato
     """
-    # Check if the Token UserId matches (PlayerID)
-    # 403: Forbidden
-    if (current_user.id != int(playerId)): 
-        return make_response(jsonify({'message': 'PlayerID Invalid. You are not authorized.'}), 403)
  
     try:
         # Ottenere l'importo dal corpo della richiesta
@@ -416,11 +415,6 @@ def update_user_currency(current_user, token, playerId):
     """
     Aggiorna la quantità di currency nel wallet di un utente specifico.
     """
-    # Check if the Token UserId matches (PlayerID)
-    # 403: Forbidden
-    #if (current_user.id != int(playerId)): 
-    #    return make_response(jsonify({'message': 'PlayerID Invalid. You are not authorized.'}), 403)
- 
     # -- (!) --- questa funzione viene usata quando auctionservice dever rimborsare un utente.
  
     try:
