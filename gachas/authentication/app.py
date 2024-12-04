e61863c1ffdea5fc00c592119f6ff1cb50b8a293
davide-di-rocco-88 <davide.dirocco88@gmail.com>
2024-12-04 10:40:51 +0100

    Implemented timeout in all functions

--- gachas/authentication/app.py
+++ gachas/authentication/app.py
@@ -103,7 +103,7 @@ def token_required(f):
                 admin_service_url = "https://admin_service:5000/admin_service/verify_admin"
                 payload = {'admin_username': username}
                 try:
-                    response = requests.get(admin_service_url, headers={'x-access-token': token}, verify=False)
+                    response = requests.get(admin_service_url, headers={'x-access-token': token}, verify=False,timeout=5)
                     # Ensure status code is 200
                     if response.status_code != 200:
                         return jsonify(response.json()), 403
@@ -117,6 +117,9 @@ def token_required(f):
                     }
                     
                     return f(admin_data, token, *args, **kwargs)
+                except Timeout:
+                    # If the request times out, return an appropriate message
+                    return make_response(jsonify({'message': 'Admin service is temporarily unavailable'}), 503)  # Service Unavailable
                 except requests.exceptions.RequestException as e:
                     # Gestisci errori durante la richiesta HTTP
                     return jsonify({'message': 'Error validating admin token!', 'error': str(e)}), 500
